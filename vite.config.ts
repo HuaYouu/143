297182b195d92228169ebd282b4e0883ebad2000
aigedu <storagenhanlh@gmail.com>
2025-11-26 22:59:53 +0700

    update package and vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: '/lovechronicle-2025/', // cần để GitHub Pages load đúng
+    base: '/143/', // cần để GitHub Pages load đúng
     server: {
       port: 3000,
       host: '0.0.0.0',
